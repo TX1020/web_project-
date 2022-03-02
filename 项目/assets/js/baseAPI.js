b08715d70fd9a1779f8259cb874dc75c95fb150a
TX1020 <740543029@qq.com>
2022-03-02 22:17:07 +0800

    完成个人中心开发

--- 项目/assets/js/baseAPI.js
+++ 项目/assets/js/baseAPI.js
@@ -1,5 +1,4 @@
 $.ajaxPrefilter(function (options) {
-    console.log(options)
     options.url = 'http://www.liulongbin.top:3007' + options.url
     if (options.url.indexOf('/my/') !== -1) {
         options.headers = {
